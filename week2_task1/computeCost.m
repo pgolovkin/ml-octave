dd6038e34606ae95275e1152385b04eb1619edba
Pavel Golovkin <pavel.golovkin@bsc-ideas.com>
2016-05-13 22:03:18 +0300

    Week 2, task 2

--- week2_task1/computeCost.m
+++ week2_task1/computeCost.m
@@ -12,11 +12,10 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
-
+result = X * theta;
+sigma = result - y;
+sigma = sigma .^ 2;
+r = sum(sigma(:));
+J = r / (2 * m);
 % =========================================================================
-
 end
