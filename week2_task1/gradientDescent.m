51844b305b3e044409daf54fd6c3ea3bdce8059e
Pavel Golovkin <pavel.golovkin@bsc-ideas.com>
2016-05-14 09:16:40 +0300

    week 2 task3

--- week2_task1/gradientDescent.m
+++ week2_task1/gradientDescent.m
@@ -16,13 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    
+    theta = theta - ((1/m) * ((X * theta) - y)' * X)' * alpha;
+ 
     % ============================================================
 
     % Save the cost J in every iteration    
